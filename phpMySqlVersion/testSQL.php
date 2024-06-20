2b67899427d951aadc0e6e52f92b08ba3c597279
lyc32 <liyc930826@gmail.com>
2024-06-20 13:06:46 -0500

    php mySQLi

--- phpMySqlVersion/testSQL.php
+++ phpMySqlVersion/testSQL.php
@@ -1,16 +1,17 @@
 <?php   
-   $connection = new mysqli("127.0.0.1", "root", "qwer1234", "project");
+   $connection = new mysqli("127.0.0.1", "root", "", "project");
    if($connection->connect_error)
    {
       echo("connect DB ERROR");
    }
    else 
    {
-      $result = $connection->query("SELECT * FROM Category WHERE state ='active'");
-      while ($row = $result->fetch_assoc())
-      {
-         echo($row['id']." ".$row['category_name']." ".$row['link']." ".$row['state']."<br>");
-      }
+      $result = $connection -> query("SELECT * FROM Category WHERE state ='active'");
+      echo("get result");
+      //while ($row = $result->fetch_assoc())
+      //{
+      //   echo($row['id']." ".$row['category_name']." ".$row['link']." ".$row['state']."<br>");
+      //}
    }
    
 ?>
