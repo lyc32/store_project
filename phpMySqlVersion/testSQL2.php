12856decdd88663ee89630fa4e27e7d3408c7248
lyc32 <liyc930826@gmail.com>
2024-06-20 13:18:17 -0500

    PDO connect DB

--- phpMySqlVersion/testSQL2.php
+++ phpMySqlVersion/testSQL2.php
@@ -1,7 +1,12 @@
 <?php
    try {
-      $conn = new PDO("mysql:host=127.0.0.1;", "root", "qwer1234");
+      $conn = new PDO("mysql:host=127.0.0.1;dbname=project", "root", "");
       echo "连接成功"; 
+      $stmt = $conn->query("SELECT * FROM CATEGORY WHERE state ='active'");
+      while ($row = $stmt->fetch()) 
+      {
+         echo $row['category_name']."<br>\n";
+      }
    }
    catch(PDOException $e)
    {
