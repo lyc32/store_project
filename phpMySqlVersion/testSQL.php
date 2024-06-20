5b5f342d0c5ee986c8c428ac3d95d01c992e1922
lyc32 <liyc930826@gmail.com>
2024-06-20 12:39:22 -0500

    php mySQL test

--- phpMySqlVersion/testSQL.php
+++ phpMySqlVersion/testSQL.php
@@ -1,8 +1,8 @@
-<?php
-   $connection = new mysqli("127.0.0.1", "root", "qwer1234", "project");
-   
-   $result = $connection->query("SELECT * FROM Category WHERE state ='active'");
-   while ($row = $result->fetch_assoc())
+<?php   
+   $connection = mysql_connect("127.0.0.1", "root", "qwer1234") or die("Unable to connect!");
+   mysql_select_db("project");
+   $result = mysql_query("SELECT * FROM Category WHERE state ='active'");
+   while($row = mysql_fetch_array($result))
    {
       echo($row['id']." ".$row['category_name']." ".$row['link']." ".$row['state']."<br>");
    }
